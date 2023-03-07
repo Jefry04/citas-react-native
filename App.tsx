e4ac59cfdcce29c6a06f7394d9ab1f6eee8a79de
Jefferson <jefry04@gmail.com>
2023-03-07 17:09:12 -0500

    create form

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, StyleSheet, Pressable} from 'react-native';
+import Form from './src/components/Form';
+
+function App(): JSX.Element {
+  const [isOPenModal, setIsOpenModal] = useState<boolean>(false);
+
+  const newDateHandler = () => setIsOpenModal(true);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Administrador de citas {''}
+        <Text style={styles.titleBold}>Veterinaria</Text>
+      </Text>
+      <Pressable onPress={newDateHandler} style={styles.btnNuevaCita}>
+        <Text style={styles.btnTextnewDate}>NUEVA CITA</Text>
+      </Pressable>
+      <Form isOpenModal={isOPenModal} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  titleBold: {
+    fontWeight: '900',
+    color: '#6028D9',
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6d28d9',
+    padding: 15,
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  btnTextnewDate: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '900',
+  },
+});
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,129 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  Modal,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+  ScrollView,
+} from 'react-native';
+
+interface IFormProps {
+  isOpenModal: boolean;
+}
+
+const Form: React.FC<IFormProps> = ({isOpenModal}) => {
+  const [patient, setPatient] = useState('');
+  const [owner, setOwner] = useState('');
+  const [mail, setMail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [symptoms, setSymptoms] = useState('');
+
+  return (
+    <Modal animationType="slide" visible={isOpenModal}>
+      <SafeAreaView style={styles.contenido}>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            Nueva {''}
+            <Text style={styles.tituloBold}>Cita</Text>
+          </Text>
+          <View style={styles.content}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Paciente"
+              placeholderTextColor={'#666'}
+              value={patient}
+              onChangeText={setPatient}
+            />
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Propietario"
+              placeholderTextColor={'#666'}
+              value={owner}
+              onChangeText={setOwner}
+            />
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.label}>Email Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email Propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="email-address"
+              value={mail}
+              onChangeText={setMail}
+            />
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.label}>Telefono Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Telefono propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="number-pad"
+              value={phone}
+              onChangeText={setPhone}
+              maxLength={10}
+            />
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.label}>Sintomas</Text>
+            <TextInput
+              style={[styles.input, styles.symptomsInput]}
+              placeholder="Sintomas"
+              placeholderTextColor={'#666'}
+              value={symptoms}
+              onChangeText={setSymptoms}
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    backgroundColor: '#6d28d9',
+    flex: 1,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#FFF',
+  },
+  tituloBold: {
+    fontWeight: '900',
+  },
+  content: {
+    marginTop: 10,
+    marginHorizontal: 30,
+    marginBottom: 10,
+  },
+  label: {
+    color: '#fff',
+    marginBottom: 10,
+    marginTop: 5,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+  },
+  symptomsInput: {
+    height: 100,
+  },
+});
+
+export default Form;
